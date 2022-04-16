6843aa9a98a02a0c62479905f65546a0fc0ac267
unknown <hedgy134117@gmail.com>
2022-04-16 18:17:01 -0400

    working predictions

--- model.py
+++ model.py
@@ -3,38 +3,60 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 
-# The data is divided up into 4 categories, the training data and the testing data
-# where X is the inputs and Y is the outputs
-(x_train, y_train), (x_test, y_test) = datasets.mnist.load_data()
 
-fig = plt.figure()
-fig.set_size_inches(10, 10)
-for i in range(16):
-    plt.subplot(4, 4, i + 1)
-    plt.imshow(x_train[i], cmap="gray")
-    plt.title(y_train[i])
-plt.tight_layout()
-plt.show()
+def create_model():
+    # The data is divided up into 4 categories, the training data and the testing data
+    # where X is the inputs and Y is the outputs
+    (x_train, y_train), (x_test, y_test) = datasets.mnist.load_data()
 
-x_train = x_train / 255
-x_test = x_test / 255
+    fig = plt.figure()
+    fig.set_size_inches(10, 10)
+    for i in range(16):
+        plt.subplot(4, 4, i + 1)
+        plt.imshow(x_train[i], cmap="gray")
+        plt.title(y_train[i])
+    plt.tight_layout()
+    plt.show()
 
-y_train = tf.keras.utils.to_categorical(y_train, 10)
-y_test = tf.keras.utils.to_categorical(y_test, 10)
+    x_train = x_train / 255
+    x_test = x_test / 255
 
-model = models.Sequential(
-    [
-        layers.Flatten(input_shape=(28, 28)),
-        layers.Dense(16, activation="relu"),
-        layers.Dense(16, activation="relu"),
-        layers.Dense(10, activation="softmax"),
-    ]
-)
+    y_train = tf.keras.utils.to_categorical(y_train, 10)
+    y_test = tf.keras.utils.to_categorical(y_test, 10)
 
-model.summary()
+    model = models.Sequential(
+        [
+            layers.Flatten(input_shape=(28, 28)),
+            layers.Dense(16, activation="relu"),
+            layers.Dense(16, activation="relu"),
+            layers.Dense(10, activation="softmax"),
+        ]
+    )
 
-model.compile(loss="categorical_crossentropy", metrics=["accuracy"])
+    model.summary()
 
-history = model.fit(x_train, y_train, epochs=5)
+    model.compile(loss="categorical_crossentropy", metrics=["accuracy"])
 
-model.save("./model")
+    history = model.fit(x_train, y_train, epochs=5)
+
+    model.save("./model.h5")
+
+
+def predict(data):
+    model = tf.keras.models.load_model("./model.h5")
+    prediction = model.predict(np.array([data]))
+    bestPrediction = 0
+    bestPredictionNum = None
+    for i in range(len(prediction[0])):
+        pred = np.around(prediction[0][i], 3)
+        if pred > bestPrediction:
+            bestPrediction = pred
+            bestPredictionNum = i
+        print(f"Confidence of a {i}: {pred}")
+    print(
+        f"Best prediction: {bestPredictionNum}, {round(bestPrediction * 100)}% Confidence"
+    )
+
+
+if __name__ == "__main__":
+    create_model()
--- viewer.py
+++ viewer.py
@@ -1,4 +1,6 @@
 import pygame
+import numpy as np
+import model
 
 
 pygame.init()
@@ -29,8 +31,8 @@ class Grid:
 
     def create_grid(self) -> list:
         rects = []
-        for y in range(self.y, self.height, self.step):
-            for x in range(self.x, self.width, self.step):
+        for y in range(self.y, self.height + self.step, self.step):
+            for x in range(self.x, self.width + self.step, self.step):
                 rects.append(Cell(x, y, self.step, self.step, pygame.Color("#000000")))
         return rects
 
@@ -41,22 +43,28 @@ class Grid:
 
     def draw_border(self) -> None:
         pygame.draw.line(
-            window, pygame.Color("#FFFFFF"), (self.x, self.y), (self.width, self.y)
+            window,
+            pygame.Color("#FFFFFF"),
+            (self.x, self.y),
+            (self.width + self.step, self.y),
         )
         pygame.draw.line(
             window,
             pygame.Color("#FFFFFF"),
-            (self.width, self.y),
-            (self.width, self.height),
+            (self.width + self.step, self.y),
+            (self.width + self.step, self.height + self.step),
         )
         pygame.draw.line(
             window,
             pygame.Color("#FFFFFF"),
-            (self.width, self.height),
-            (self.x, self.height),
+            (self.width + self.step, self.height + self.step),
+            (self.x, self.height + self.step),
         )
         pygame.draw.line(
-            window, pygame.Color("#FFFFFF"), (self.x, self.height), (self.x, self.y)
+            window,
+            pygame.Color("#FFFFFF"),
+            (self.x, self.height + self.step),
+            (self.x, self.y),
         )
 
     def get_mouse_index(self) -> int:
@@ -66,20 +74,54 @@ class Grid:
         return 0
 
     def highlight_cell(self, index) -> None:
+        self.rects[index - 29].color = pygame.Color("#FFFFFF")
+        self.rects[index - 28].color = pygame.Color("#FFFFFF")
+        self.rects[index - 27].color = pygame.Color("#FFFFFF")
+
+        self.rects[index - 1].color = pygame.Color("#FFFFFF")
         self.rects[index].color = pygame.Color("#FFFFFF")
+        self.rects[index + 1].color = pygame.Color("#FFFFFF")
+
+        self.rects[index + 27].color = pygame.Color("#FFFFFF")
+        self.rects[index + 28].color = pygame.Color("#FFFFFF")
+        self.rects[index + 29].color = pygame.Color("#FFFFFF")
 
     def unhighlight_cell(self, index) -> None:
         self.rects[index].color = pygame.Color("#000000")
 
+    def clear(self) -> None:
+        for rect in self.rects:
+            rect.color = pygame.Color("#000000")
 
-step = 10
-grid = Grid(step, step, 28 * step, 28 * step, 10)
+    def generate_np_array(self) -> np.ndarray:
+        rowLength = self.width // self.step
+        rows = self.height // self.step
+        arr = [[] for _ in range(rows)]
+        i = 0
+        rowI = 0
+        for cell in self.rects:
+            arr[rowI].append(((cell.color.r + cell.color.g + cell.color.b) // 255) / 3)
+            i += 1
+            if i >= rowLength:
+                rowI += 1
+                i = 0
+        return np.asarray(arr)
+
+
+step = 20
+grid = Grid(step, step, 28 * step, 28 * step, step)
+
 
 while is_running:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                model.predict(grid.generate_np_array())
+            if event.key == pygame.K_BACKSPACE:
+                grid.clear()
 
     # window.blit(background, (0, 0))
 
